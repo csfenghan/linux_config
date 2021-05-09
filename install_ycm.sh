e755a0223656b5d6897d277ea44b93b67a70640b
fenghan <csfenghan@outlook.com>
2021-05-09 12:36:42 +0800

    部分改动

--- install_ycm.sh
+++ install_ycm.sh
@@ -1,7 +1,7 @@
 
 # 安装依赖
 sudo apt update
-sudo apt install clang cmake vim python3 python3-dev
+sudo apt install clang cmake vim python3 python3-dev python3-pylint-common clang-format  universal-ctags
 
 # 安装ycm
 sudo apt install vim-addon-manager vim-youcompleteme 
