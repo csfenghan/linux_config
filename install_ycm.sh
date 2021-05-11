670015aa39f3eaefd1a27f32bbf70fb2450d1555
fenghan <csfenghan@foxmail.com>
2021-05-11 13:41:12 +0800

    删除了安装ycm的文件

--- install_ycm.sh
+++
@@ -1,7 +0,0 @@
-
-# 安装依赖
-sudo apt install clang cmake vim python3 python3-dev python3-pylint-common clang-format  universal-ctags
-
-# 安装ycm
-sudo apt install vim-addon-manager vim-youcompleteme 
-vim-addons install youcompleteme
